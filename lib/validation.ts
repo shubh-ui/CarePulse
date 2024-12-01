3cd001b2ff7573208179425d4b8701826143958d
Shubham Patil <shubhpatil8510@gmail.com>
2024-12-01 23:39:38 +0530

    Zod validation fix

--- lib/validation.ts
+++ lib/validation.ts
@@ -3,6 +3,6 @@ import { z } from "zod";
 export const UserFormValidation = z.object({
     name: z.string().min(2, "Name must be at least 2 characters.",)
     .max(50, "Name must be at most 50 characters."),
-    email:z.string().email("INvalid email address"),
+    email:z.string().email("Invalid email address"),
     phone:z.string().refine((phone) => /^\+?[1-9]\d{1,14}$/.test(phone),'Invalid phone number')
   })
